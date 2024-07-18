6d74d14621ebfd38d1dccc3825f7921a7d382aba
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2024-07-19 02:35:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import requests
+
+st.set_page_config(
+    page_title="Riddle Riot",
+    page_icon="icon.png",
+    menu_items={
+        "About":"Experience the excitement of solving riddles and brain teasers. Join the riot of fun and test your problem-solving skills."
+    }
+)
+
+st.write("<h2 style='color:#3FBA8C;'>Challenge Your Mind with Intriguing Puzzles.</h2>",unsafe_allow_html=True)
+
+btn=st.button("Generate Riddle")
+
+if btn:
+    api_url = 'https://api.api-ninjas.com/v1/riddles'
+    response = requests.get(api_url, headers={'X-Api-Key': '2jWCY0dASiPZc7RLybXvXA==R9oC0XPKPWiGJ6k6'})
+    if response.status_code == requests.codes.ok:
+        data=response.json()
+        print(data)
+        title=data[0]["title"]
+        question=data[0]["question"]
+        answer=data[0]["answer"]
+        st.write(f"<p style=color:#46B5E3;font-size:32px;>Title: {title}</p>",unsafe_allow_html=True)
+        st.write(f"<p style=font-size:22px;>Question: {question}</p>",unsafe_allow_html=True)
+        with st.expander("Show Answer"):
+            st.write(f"<p style=font-size:20px;>{answer}</p>",unsafe_allow_html=True)
